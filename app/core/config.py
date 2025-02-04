279ee7d3f1691b0c77d8180629981f4646378455
Seyoun Kim <129013571+syk25@users.noreply.github.com>
2025-02-05 00:40:40 +0900

    🔧 [Fix] 개발환경과 배포환경의 구분 후 서버와 데이터베이스 연결 (#29)

--- app/core/config.py
+++ app/core/config.py
@@ -15,13 +15,22 @@ class Settings(BaseSettings):
     # 환경 설정 (default: development)
     ENV: str = os.getenv("ENV", "development")
 
-    # MySQL 환경 변수
+    # ✅ MySQL 환경 변수
     MYSQL_ROOT_PASSWORD: str
     MYSQL_DATABASE: str
     MYSQL_USER: str
     MYSQL_PASSWORD: str
-    MYSQL_PORT: int
-    MYSQL_HOST: str = os.getenv("MYSQL_HOST", "127.0.0.1")  # 기본값: 로컬
+
+    # ✅ MySQL 포트 & 호스트 설정 (환경에 따라 자동 변경)
+    MYSQL_PORT: int = (
+        3309 if ENV == "development" else 3306
+    )  # 로컬(Docker)은 3309, 배포(RDS)는 3306
+
+    MYSQL_HOST: str = (
+        "host.docker.internal"
+        if ENV == "development"
+        else os.getenv("MYSQL_HOST", "127.0.0.1")
+    )  # 로컬은 Docker 내부 접근, 배포는 RDS 주소 사용
 
     # 카카오 API 환경 변수
     KAKAO_CLIENT_ID: str
